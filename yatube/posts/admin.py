906ced697175b86376ded7853402e44014fed49e
Роман Дегтярев <r.degtyarev5@gmail.com>
2023-03-08 17:00:02 +0300

    superuser, migrations

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author')  #отображение в админке
+    search_fields = ('text',)  #поиск по тексту
+    list_filter = ('pub_date',) #фильтр по дате
+    empty_value_display = '-пусто-'
+
+
+
+admin.site.register(Post, PostAdmin)
+
+
+
 # Register your models here.
