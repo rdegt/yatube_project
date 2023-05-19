e19b71769a3cd8a13e994775b3a812f3e10a536f
Роман Дегтярев <r.degtyarev5@gmail.com>
2023-05-20 02:16:07 +0300

    Восстановление, изменение пароля

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,7 +1,11 @@
 # Импортируем из приложения django.contrib.auth нужный view-класс
-from django.contrib.auth.views import LogoutView, LoginView, PasswordResetView
+from django.contrib.auth.views import LogoutView, LoginView, PasswordResetView 
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import PasswordResetDoneView
+from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path
 from . import views
+from django.urls import reverse_lazy
 
 app_name = 'users'
 
@@ -20,7 +24,35 @@ urlpatterns = [
     ),
      path(
         'password_reset/',
-        PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+        PasswordResetView.as_view(template_name='users/password_reset_form.html', 
+                                  success_url = reverse_lazy('users:password_reset_done')),
         name='password_reset'
     ),
+    path(
+        'password_reset/done/',
+        PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html', 
+                                         success_url = reverse_lazy('users:reset_complete')),
+        name='reset_letter'
+    ),
+    path(
+        'reset/done/',
+        PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        name='reset_complete'
+    ),
+    path(
+        'password_change/',
+        PasswordChangeView.as_view(template_name='users/password_change_form.html',success_url = reverse_lazy('users:password_change_done')),
+        name='password_change'
+    ),
+    path(
+        'password_change/done/',
+        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        name='password_change_done'
+    ),
+
 ]
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Profile
 
 # будем ссылаться на неё во view-классе
 from .forms import CreationForm
+from django.contrib.auth.forms import PasswordChangeForm
 
 class SignUp(CreateView):
     model = Profile
@@ -18,3 +19,9 @@ class SignUp(CreateView):
     success_url = reverse_lazy('posts:index')
 
     #Данные отправленные через форму будут переданы в модель User и сохранены в БД
+
+class PasswordChange(UpdateView):
+    model = Profile
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('users:password_change_done')
+    template_name = 'users/password_change_form.html'
